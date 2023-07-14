b696cfce8849586c4539baa6930badb121871be4
qrunchy <nearvegnugen@gmail.com>
2023-07-13 19:41:12 -0500

    First commit

---
+++ load.js
@@ -0,0 +1,5 @@
+$(function(){
+    $("header").load("header.html"); 
+    $("footer").load("footer.html"); 
+    $("nav").load("sidebar.html"); 
+});
---
+++ simplelightbox/src/test.js
@@ -0,0 +1,8 @@
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+	// do something…
+});
+
+gallery.on('error.simplelightbox', function (e) {
+	console.log(e); // some usefull information
+});
